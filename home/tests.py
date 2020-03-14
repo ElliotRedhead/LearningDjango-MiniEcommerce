868e83c2147b8507d4808ea0570b452a16d4d5b7
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-14 13:53:02 +0000

    Moved product tests to product folder.

--- home/tests.py
+++ home/tests.py
@@ -1,14 +1,4 @@
 from django.test import TestCase
-from .models import Product
 
 
 # Create your tests here.
-class ProductTests(TestCase):
-    """
-    Defining the  tests to be ran against
-    the Post models.
-    """
-
-    def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
