cd7101b5bb8fa4ad4d0fd72e7a22c00407c26b2c
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-17 22:42:10 +0000

    Standardised accounts URLs.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, registration, user_profile
-from accounts import url_reset
+from . import url_reset
+from .views import registration, user_profile, logout, login
 
 urlpatterns = [
     url(r'^logout/$', logout, name="logout"),
     url(r'^login/$', login, name="login"),
-    url(r'^register/$', registration, name="registration"),
+    url(r'^register/$', registration, name="register"),
     url(r'^profile/$', user_profile, name="profile"),
     url(r'^password-reset/', include(url_reset))
 ]
